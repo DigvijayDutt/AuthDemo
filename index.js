664e751b2a73bb104515919d45d5d608f5706b11
DigvijayDutt <duttdigvijay@gmail.com>
2025-04-14 16:47:31 +0530

    login and signup skeleton

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const ex= require('express');
 const app = ex();
 const User = require('./models/user');
 const mongo = require('mongoose');
+const bc = require('bcrypt');
+
+app.use(ex.urlencoded({extended:true}));
 
 mongo.connect('mongodb://127.0.0.1:27017/authdemo')
     .then(()=>{
@@ -15,8 +18,37 @@ mongo.connect('mongodb://127.0.0.1:27017/authdemo')
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.get('/',(req,res)=>{
+    res.send("Home");
+})
+
 app.get('/register', (req,res)=>{
     res.render('register');
+})
+
+app.post('/register', async(req,res)=>{
+    const {password, username} = req.body;
+    const hash = await bc.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash,
+    })
+    await user.save();
+    res.redirect('/');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+app.post('/login',async(req,res)=>{
+    const {username,password} = req.body;
+    const user = await User.findOne({username});
+    const check = await bc.compare(password, user.password);
+    if(check){
+        res.redirect('/');
+    }else{
+        res.send("Try again");
+    }
 })
 
 app.get('/secret', (req,res)=>{
